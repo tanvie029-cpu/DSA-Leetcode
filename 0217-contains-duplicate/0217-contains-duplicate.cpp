ee981f2a121f02c109d162d0c694dab42b464676
tanvibyte <tanvie029@gmail.com>
2026-07-09 00:00:10 +0530

    Time: 73 ms (20.04%), Space: 111.2 MB (13.99%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        unordered_set<int> seen;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (seen.count(nums[i])) {
+                return true;
+            }
+            else {
+                seen.insert(nums[i]);
+            }
+        }
+
+        return false;
+    }
+};
